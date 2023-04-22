23703bb3110aa951c8b7e164dadaecbba321b1f2
leandro <leandro.devmcz@gmail.com>
2023-04-21 23:16:19 -0300

    imput

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -2,19 +2,51 @@ import { Container } from './styles';
 
 import { Header } from '../../components/Header';
 import { Form } from 'react-router-dom';
+import { useState } from 'react';
 
 export function Contact() {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
+
+  function sendEmail(){
+    alert("")
+  }
   
   return (
     <Container>
       <Header/>
-        <h1>Contato</h1>
-        <main>
-          <input type="text" placeholder='Nome' required/>
-          <input type="email" placeholder='Email' required/>
-          <input type="text" placeholder='Digite aqui' minLength={100} required/>
-          <button>Enviar</button>
-        </main>
+        <h1 className='title'>Contato</h1>
+        <div className="container">
+
+          <form className="form" onSubmit={() => {}}>
+            <input
+             className="input"
+             type="text"
+             placeholder="Digite seu nome"
+             onChange={(e) => {setEmail(e.target.value)}}
+             value={name}
+            />
+
+            <input
+             className="input"
+             type="email"
+             placeholder="Digite seu email"
+             onChange={(e) => {setName(e.target.value)}}
+             value={email}
+            />
+
+            <textarea
+             className="textarea"
+             type="text"
+             placeholder="Digite sua mensagem..."
+             onChange={(e) => {setMessage(e.target.value)}}
+             value={message}
+            />
+          </form>
+        </div>
+
+        <input className='button' type="submit" value="Enviar" />
     </Container>
   )
 };
--- src/pages/contact/styles.js
+++ src/pages/contact/styles.js
@@ -1,37 +1,72 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  a { 
-   color: currentColor;
-    }
-
-h1 {
-  display: flex;
-  justify-content: center;
-
-  font-size: 30px;
-}
-
-main {
   display: flex;
   flex-direction: column;
-  //background-color: #9aa9af;
+  align-items: center;
   
-  margin: 20px auto;
-
-  //height: 150px;
-  width: 300px;
-  //padding: 20px;
+  a { 
+  color: currentColor;
 }
 
-button {
+h1 {
+  font-size: 30px;
+  margin-top: 20px;
+}
+
+form {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  margin-bottom: 10px;
+  
+  margin: 20px auto;
+  
+  width: 280px;
+  
+}
+
+.container {
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  justify-content: flex-start;
+  
+  background-color: #d4e3ee;
+  
+  width: 300px;
+  //height: 370px;
+  border-radius: 5px;
+  
+  box-shadow: 0px 11px 35px 2px rgba(0, 0, 0, 0.14);
+}
+
+.input {
+  padding: 5px;
+  border: none;
+  border-radius: 5px;
+  margin-bottom: 10px;
+  
+}
+
+.textarea {
+  height: 150px;
+  border: none;
+  border-radius: 5px;
+  margin-bottom: 10px;
+}
+
+.button {
 
   font-weight: 700;
-  height: 30px;
+  height: 40px;
+  width: 300px;
   border-radius: 3px;
   border: 1px;
 
-  margin-top: 5px;
+  margin-top: 10px;
 
   border: 1px;
   border-style: solid;
@@ -40,7 +75,7 @@ button {
   
 }
 
-button:hover {
+.button:hover {
   cursor: pointer;
   border: none;
   color: #FFFF;
@@ -48,10 +83,14 @@ button:hover {
   background: linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(103,106,214,1) 64%, rgba(0,212,255,1) 100%);
 }
 
-
-
-
-
+@media (min-width: 1024px) {
+.container {
+  width: 350px;
+}
+.button {
+  width: 350px;
+}
+}
 
 
 `;
