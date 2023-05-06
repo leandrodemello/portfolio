d1ead4c870ac92f6e54b25753d6ef0e36ecf55bb
leandro <leandro.devmcz@gmail.com>
2023-05-06 00:28:30 -0300

    img

--- src/styles/global.js
+++ src/styles/global.js
@@ -15,7 +15,7 @@ export default createGlobalStyle`
  body {
   color: #FFFF;
   //background-color: #262628;
-  background: url("public/online-storecv.png") no-repeat fixed;
+  background: url("src/asset/online-storecv.png") no-repeat fixed;
   //background: url("public/online storecc.png") no-repeat fixed;
   background-size: cover;
   
